11e0e88414c3a30209cb7df3dfa40f8a2bc7c8d1
sophiakarski <sophiakarski@gmail.com>
2023-04-20 12:39:33 +0100

    Initial change

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -8,7 +8,7 @@ public class IndexPage extends HtmlPage {
   protected void writeContentTo(PrintWriter writer) {
     writer.println(
         "<h1>Welcome!</h1>"
-            + "<p>Please enter your query in the box below: "
+            + "<p>Hi there! Please enter your query in the box below: "
             + "<form><input type=\"text\" name=\"q\" />"
             + "<input type=\"submit\">"
             + "</form>"
